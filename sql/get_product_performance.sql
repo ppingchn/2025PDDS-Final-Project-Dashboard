d4820c649644cca64f9df3f2d14747ffc2864bed
ppingchn <choonhanunt@gmail.com>
2025-12-06 15:28:17 +0800

    Relocted of the file

--- sql/get_product_performance.sql
+++ sql/get_product_performance.sql
@@ -1,7 +1,7 @@
 SELECT
 p.category,
 SUM(oi.quantity) AS total_sales_volume,
-AVG(r.rating) AS average_customer_rating,
+AVG(r.rating) AS average_customer_rating
 FROM Products as p
 JOIN Order_Items as oi ON p.product_id = oi.product_id
 JOIN Reviews as r on oi.order_id = r.order_id
