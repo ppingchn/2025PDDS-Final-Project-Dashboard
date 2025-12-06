5e0e9939941faf772b420cde8c183eb10d161323
ppingchn <choonhanunt@gmail.com>
2025-12-06 12:54:22 +0800

    Create the SQL query

---
+++ visual/sql/get_product_performance.sql
@@ -0,0 +1,9 @@
+SELECT
+p.category,
+SUM(oi.quantity) AS total_sales_volume,
+AVG(r.rating) AS average_customer_rating,
+FROM Products as p
+JOIN Order_Items as oi ON p.product_id = oi.product_id
+JOIN Reviews as r on oi.order_id = r.order_id
+GROUP BY p.category
+ORDER BY total_sales_volume DESC;
