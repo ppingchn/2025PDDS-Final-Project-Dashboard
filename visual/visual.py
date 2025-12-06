9043c39c60c7361009a8217aefd3f2ad4d418ba8
ppingchn <choonhanunt@gmail.com>
2025-12-06 12:24:47 +0800

    Create the visual.py for putting the visualize function

---
+++ visual/visual.py
@@ -0,0 +1,25 @@
+# Library imports & Setup
+import plotly.express as px
+import plotly.graph_objects as go
+import pandas as pd
+
+# Global Variables (if any)
+
+
+
+# Visualization Functions for Tab 1: Strategy Tab
+
+# Visualize of Global Revenue Map (Choropleth)
+
+
+
+# Visualize of Customer Value Matrix (Scatter Plot)
+
+
+# Visualization Functions for Tab 2: Operation Tab
+
+# Visualize of Product Issues Pareto (Bar + Line)
+
+
+
+# Visualize of Service Quality Over Time (Line Chart)
