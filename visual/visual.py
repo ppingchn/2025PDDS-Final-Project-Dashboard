0be79bc6667f84b0e0249929a35851a930249942
ppingchn <choonhanunt@gmail.com>
2025-12-06 14:43:29 +0800

    connected the sql query for data

--- visual/visual.py
+++ visual/visual.py
@@ -2,6 +2,7 @@
 import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
+from ..db_service import get_connection, extract_query_from_file
 
 # Global Variables (if any)
 
@@ -19,7 +20,24 @@ import pandas as pd
 # Visualization Functions for Tab 2: Operation Tab
 
 # Visualize of Product Issues Pareto (Bar + Line)
+def get_product_performance():
+    # Get DB Connection
+    conn = get_connection()
+    
+    # Extract SQL Query
+    query = extract_query_from_file("get_product_performance.sql")
+    if query is None:
+        return None  # Exit if query could not be read
+
+    # Execute Query and Fetch Data
+    df = pd.read_sql_query(query, conn)
+    
+    # Close Connection
+    conn.close()
+    
+    # Visualization Part
 
 
+    return df
 
 # Visualize of Service Quality Over Time (Line Chart)
