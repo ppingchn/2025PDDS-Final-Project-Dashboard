4246b7334d550ff80eb04b71ff2d85f35d6bec71
ppingchn <choonhanunt@gmail.com>
2025-12-11 20:15:35 +0800

    Add index (all country) value

--- visual.py
+++ visual.py
@@ -14,6 +14,7 @@ def get_country_list():
     conn.close()
 
     options = [{'label': country, 'value': country} for country in df['country']]
+    options.insert(0, {'label': 'All Countries', 'value': None})
     return options
 
 # Visualization Functions for Tab 1: Strategy Tab
@@ -44,7 +45,6 @@ def get_product_performance():
     conn.close()
     
     # Visualization Part
-    # Make subplots with secondary y-axis
     fig = make_subplots(specs=[[{"secondary_y": True}]])
 
     # Bar Chart -- Total Sales Volume
