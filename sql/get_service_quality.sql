9b961c35373ff03a362564ea1ed2bb5f598013da
elliottoh0930 <elliottoh@gapp.nthu.edu.tw>
2025-12-11 21:00:14 +0800

    Add SQL command

---
+++ sql/get_service_quality.sql
@@ -0,0 +1,16 @@
+SELECT
+    STRFTIME('%Y-%m-01', o.order_date) AS month,
+    AVG(julianday(o.delivery_date) - julianday(o.order_date)) AS avg_shipping_days,
+    AVG(r.rating) AS avg_review_score
+FROM
+    Orders o
+LEFT JOIN
+    Reviews r
+ON
+    o.order_id = r.order_id
+WHERE
+    o.delivery_date IS NOT NULL
+GROUP BY
+    STRFTIME('%Y-%m-01', o.order_date)
+ORDER BY
+    month;
