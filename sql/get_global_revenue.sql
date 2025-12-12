6f6ac7b46dbbc8affdb7840df38807f557017e77
pim-achara <pim.acharawatchala@gmail.com>
2025-12-12 15:39:21 +0800

    Modify: Updated the new SQL command adding only necessary section

--- sql/get_global_revenue.sql
+++ sql/get_global_revenue.sql
@@ -1,10 +1,11 @@
 SELECT
-        c.country,
-        o.order_date,
-        o.delivery_date,
-        oi.quantity,
-        oi.unit_price,
-        o.order_id
+    c.country,
+    SUM(oi.quantity * oi.unit_price) AS total_revenue,
+    ROUND(AVG(JULIANDAY(o.delivery_date) - JULIANDAY(o.order_date)), 1) AS avg_delivery_time,
+    ROUND(SUM(oi.quantity * oi.unit_price) / COUNT(DISTINCT o.order_id), 0) AS avg_basket_size
+
 FROM Orders AS o
 JOIN Customers AS c ON o.customer_id = c.customer_id
 JOIN Order_Items AS oi ON o.order_id = oi.order_id
+GROUP BY 
+    c.country;
