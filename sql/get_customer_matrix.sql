4e43649c28c5968ad7a51547e61c1b3959ff0b8c
Kittiphoom Treesam <fongg321@gmail.com>
2025-12-16 19:52:19 +0800

    Create: SQL query adding for function

---
+++ sql/get_customer_matrix.sql
@@ -0,0 +1,11 @@
+SELECT 
+		c.customer_id, 
+		c.country,
+		strftime('%Y', o.order_date) AS year, 
+		strftime('%Y-%m', o.order_date) AS month, 
+		SUM(oi.quantity * oi.unit_price) AS total_spent, 
+		COUNT(DISTINCT o.order_id) AS total_frequency_purchase 
+FROM Orders o JOIN Customers c ON o.customer_id = c.customer_id 
+JOIN Order_Items oi ON o.order_id = oi.order_id 
+WHERE o.order_status IN ('Pending', 'Delivered', 'Shipped') 
+GROUP BY c.customer_id, c.country, year
